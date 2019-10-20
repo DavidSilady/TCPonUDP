e20e4107ebc9c9373d7d63f5236afb869d269214
DavidSilady <david.silady@gmail.com>
2019-10-20 19:58:26 +0200

    KINDA WORKIN

--- server.py
+++ server.py
@@ -19,11 +19,14 @@ sock.bind((UDP_IP, UDP_PORT))
 TARGET_IP = "169.254.195.226"
 TARGET_PORT = 35000
 
+IS_FILE = False
+FILE_NAME = ""
+FAULTY_INDEX = 2
 RTT = 2
 MAX_THREAD_COUNT = 100
-thread_stop = []
-thread_count = 1
-timeout = []
+THREAD_STOP = []
+THREAD_COUNT = 1
+TIMEOUT = []
 BUFFER = []
 SERVER_BUFFER = []
 
@@ -31,8 +34,8 @@ SERVER_BUFFER = []
 def send_first(num_packets):
 	first = Response(num_packets, 'H')  # H - Header
 	sock.sendto(first.to_bytes(), (TARGET_IP, TARGET_PORT))
-	
-	
+
+
 # https://stackoverflow.com/a/30239138
 def multi_line_input():
 	lines = []
@@ -60,30 +63,43 @@ def send_text():
 
 
 def queue_packets(packets):
-	global thread_count
-	thread_stop.clear()
-	timeout.clear()
+	global THREAD_COUNT
+	THREAD_STOP.clear()
+	TIMEOUT.clear()
 	send_first(len(packets))
 	for packet in packets:
-		while thread_count > MAX_THREAD_COUNT:
-			print("Max Threads", thread_count)
+		while THREAD_COUNT > MAX_THREAD_COUNT:
+			print("Max Threads", THREAD_COUNT)
 			time.sleep(0.1)
-		send_t = Thread(target=send_thread, args=[packet], daemon=True)
+		send_t = Thread(target=sender_thread, args=[packet], daemon=True)
 		event = Event()
-		timeout.append(event)
-		thread_stop.append(False)
-		thread_count += 1
+		TIMEOUT.append(event)
+		THREAD_STOP.append(False)
+		THREAD_COUNT += 1
 		send_t.start()
 	print("---Sent---")
 
 
-def send_thread(packet):
+def sender_thread(packet, seq_num=len(TIMEOUT) - 1):
+	try:
+		seq_num = packet.sequence_number
+	except TypeError:
+		print("Sending Last")
 	b_packet = packet.to_bytes()
-	while not thread_stop[packet.sequence_number]:
+	faulty = True
+	while not THREAD_STOP[seq_num]:
 		# print("Sending:", packet.sequence_number)
-		sock.sendto(b_packet, (TARGET_IP, TARGET_PORT))
-		print("Packet", packet.sequence_number, "sent.")
-		timeout[packet.sequence_number].wait(RTT)
+		if seq_num == FAULTY_INDEX and faulty:
+			fault = packet
+			fault.payload = b'\x00\x00'
+			f_packet = fault.to_bytes()
+			faulty = False
+			sock.sendto(f_packet, (TARGET_IP, TARGET_PORT))
+			print("Faulty packet", seq_num, "sent.")
+		else:
+			sock.sendto(b_packet, (TARGET_IP, TARGET_PORT))
+			print("Packet", seq_num, "sent.")
+		TIMEOUT[seq_num].wait(RTT)
 	return
 
 
@@ -141,8 +157,8 @@ def command_listener():
 			global BUFFER_SIZE
 			BUFFER_SIZE = int(input("New buffer size: "))
 		elif command == ":clear":
-			global thread_stop
-			thread_stop.clear()
+			global THREAD_STOP
+			THREAD_STOP.clear()
 		else:
 			print("---Unknown Command---")
 
@@ -162,11 +178,8 @@ def send_alive(addr):
 	sock.sendto(packet, addr)
 
 
-def merge_buffer(complete=""):
-	global BUFFER
-	for chunk in BUFFER:
-		complete += chunk
-	return complete
+def merge_buffer():
+	return b''.join(BUFFER)
 
 
 def handle_content(data, addr):
@@ -175,11 +188,10 @@ def handle_content(data, addr):
 		print("Chunk:", packet.sequence_number)
 		send_ack(packet.sequence_number, addr)
 		BUFFER[packet.sequence_number] = packet.payload
-		if packet.sequence_number == len(BUFFER) - 1:
-			# complete = merge_buffer()
+		if not BUFFER.__contains__(None):
 			print(BUFFER)
+			print(merge_buffer())
 			BUFFER.clear()
-			# print("Message:", complete)
 	else:
 		print("Error: ", packet.payload, "|", SERVER_BUFFER[packet.sequence_number])  # debug
 		send_nak(packet.sequence_number, addr)
@@ -187,7 +199,7 @@ def handle_content(data, addr):
 
 def handle(data, addr):
 	global BUFFER
-	global thread_count
+	global THREAD_COUNT
 
 	packet = Packet.from_bytes(data)
 	packet_type = packet.packet_type
@@ -199,20 +211,29 @@ def handle(data, addr):
 		packet = Response.from_bytes(data)
 		print("Type:", packet.packet_type, packet.sequence_number)
 		if packet_type == 'a':
-			thread_stop[packet.sequence_number] = True
-			timeout[packet.sequence_number].set()
-			thread_count -= 1
+			THREAD_STOP[packet.sequence_number] = True
+			TIMEOUT[packet.sequence_number].set()
+			THREAD_COUNT -= 1
 		else:
-			timeout[packet.sequence_number].set()
+			TIMEOUT[packet.sequence_number].set()
+			TIMEOUT[packet.sequence_number].clear()
 	elif packet_type == 'k':  # keep alive
 		send_alive(addr)
-	elif packet_type == 'l':
+	elif packet_type == 'l':  # alive
 		print("Partner alive.")
-	elif packet_type == 'H':
+	elif packet_type == 'H':  # header
 		packet = Response.from_bytes(data)
-		BUFFER.clear()
-		BUFFER = [None] * packet.sequence_number
-		print("Receiving", packet.sequence_number, "packets. . .")
+		if packet.checksum == 0:
+			BUFFER.clear()
+			BUFFER = [None] * packet.sequence_number
+			print("Receiving", packet.sequence_number, "packets. . .")
+	elif packet_type == 'h':  # file header
+		packet = Packet.from_bytes(data)
+		if packet.checksum == 0:
+			global IS_FILE
+			global FILE_NAME
+			IS_FILE = True
+			FILE_NAME = packet.payload.decode()
 	else:
 		print("Unknown type")
 	print("\\--------------------------------------------------/\n")
